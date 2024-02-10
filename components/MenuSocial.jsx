a4a8148ed0c2d28d1e940662aa63ad554c53a8bc
Anti2023 <mariaantonietarivano@gmail.com>
2024-02-10 19:37:25 -0300

    arreglos menú flotante

--- components/MenuSocial.jsx
+++ components/MenuSocial.jsx
@@ -1,12 +1,66 @@
+// "use client";
+
+// import React from "react";
+// import {
+//     RiLinkedinFill,
+//     RiGithubFill,
+//     RiFacebookBoxFill
+// }
+//     from "react-icons/ri";
+// import { motion } from "framer-motion";
+// import Link from "next/link";
+// import { fadeIn } from "./utils/motionTrans";
+
+// const MenuSocial = () => {
+//     const dataSocial = [
+//         {
+//             name: "Linkedin",
+//             path: "https://www.linkedin.com/",
+//             icon: <RiLinkedinFill size="30" />,
+//         },
+//         {
+//             name: "Github",
+//             path: "https://github.com/",
+//             icon: <RiGithubFill size="30" />,
+//         },
+//         {
+//             name: "Facebook",
+//             path: "https://es-la.facebook.com/",
+//             icon: <RiFacebookBoxFill size="30" />,
+//         },
+//     ];
+
+//     return (
+//         <motion.div
+//         className="fixed bottom-0 z-30 flex flex-col items-center w-full mt-auto md:justify-center h-max md:top-1/4 md:left-5 md:w-16 md:max-w-md md:h-screen"
+//             variants={fadeIn("right", 0.5)}
+//             initial="hidden"
+//             animate="show"
+//             exit="hidden">
+
+//             <div className="flex items-center justify-between w-full px-4 py-3 md:py-8 md:flex-col md:justify-center gap-y-10 md:px-4 md:h-max bg-gray-500/50 sm:shadow-md backdrop-blur-sm md:rounded-full">
+//                 {dataSocial.map(({ name, path, icon }) => (
+//                     <div key={name}>
+//                         <Link href={path} className="group transition-all duration-300 hover:text-secondary">
+//                             <div className="absolute left-20 hidden mr-20 rounded:sm md:group-hover:flex md:group-hover:items-center">
+//                                 <div className="relative flex items-center p-2 leading-none text-secondary font-bold capitalize bg-gray-500/50 rounded-sm">{name}</div>
+//                             </div>
+//                             {icon}
+//                         </Link>
+//                     </div>
+//                 ))}
+//             </div>
+//         </motion.div>
+//     );
+// };
+
+
+// export default MenuSocial;
+
 "use client";
+
 import React from "react";
-import {
-    RiLinkedinFill,
-    RiGithubFill,
-    RiFacebookBoxFill
-}
-    from "react-icons/ri";
-import { motion } from "framer-motion";
+import { RiLinkedinFill, RiGithubFill, RiFacebookBoxFill } from "react-icons/ri";
 import Link from "next/link";
 import { fadeIn } from "./utils/motionTrans";
 
@@ -30,14 +84,14 @@ const MenuSocial = () => {
     ];
 
     return (
-        <motion.div
-        className="fixed bottom-0 z-30 flex flex-col items-center w-full mt-auto md:justify-center h-max md:top-1/4 md:left-5 md:w-16 md:max-w-md md:h-screen"
-            variants={fadeIn("right", 0.5)}
-            initial="hidden"
-            animate="show"
-            exit="hidden">
-
-            <div className="flex items-center justify-between w-full px-4 py-3 md:py-8 md:flex-col md:justify-center gap-y-10 md:px-4 md:h-max bg-gray-500/50 sm:shadow-md backdrop-blur-sm md:rounded-full">
+        <div className="fixed bottom-0 z-30 flex flex-col items-center w-full mt-auto md:justify-center h-max md:top-1/4 md:left-5 md:w-16 md:max-w-md md:h-screen">
+            <div
+                className="flex items-center justify-between w-full px-4 py-3 md:py-8 md:flex-col md:justify-center gap-y-10 md:px-4 md:h-max bg-gray-500/50 sm:shadow-md backdrop-blur-sm md:rounded-full animate-levitate"
+                variants={fadeIn("right", 0.5)}
+                initial="hidden"
+                animate="show"
+                exit="hidden"
+            >
                 {dataSocial.map(({ name, path, icon }) => (
                     <div key={name}>
                         <Link href={path} className="group transition-all duration-300 hover:text-secondary">
@@ -49,9 +103,8 @@ const MenuSocial = () => {
                     </div>
                 ))}
             </div>
-        </motion.div>
+        </div>
     );
 };
-
 
 export default MenuSocial;
